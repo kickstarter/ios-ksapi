92cd0ec590b4a6f4da9ababa3cd29004e0c2a717
Brandon Williams <mbw234@gmail.com>
2016-03-02 18:57:33 -0500

    Update playground to not use reactive extensions too.

--- KsApi.playground/Contents.swift
+++ KsApi.playground/Contents.swift
@@ -2,14 +2,15 @@ import XCPlayground
 import KsApi
 import ReactiveCocoa
 import Alamofire
+import struct Models.Category
+import enum Result.NoError
 
 XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
 
 let service = Service.shared
 
 let categories = service.fetchCategories()
-  .sort()
-  .uncollect()
+  .flatMap(FlattenStrategy.Concat) { SignalProducer<Category, ErrorEnvelope>(values: $0) }
   .filter { c in c.isRoot }
   .replayLazily(1)
 
@@ -21,7 +22,7 @@ categories
 
 categories
   .map { c in DiscoveryParams(category: c, sort: .Popular, perPage: 1) }
-  .mergeMap(service.fetchProject)
+  .flatMap(.Merge, transform: service.fetchProject)
   .map { p in (p.name, p.category.name) }
   .startWithNext { data in
     print("Project -----> \(data)")
