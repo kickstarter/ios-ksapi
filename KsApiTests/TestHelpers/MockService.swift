1ad8d3773a38863a809da26e92ebfe7b107c762b
Brandon Williams <mbw234@gmail.com>
2016-03-20 18:13:02 -0400

    Extract test helpers to their own framework so they can be used elsewhere.

---
+++ KsApiTests/TestHelpers/MockService.swift
@@ -0,0 +1,108 @@
+@testable import KsApi
+@testable import Models
+@testable import Models_TestHelpers
+import ReactiveCocoa
+
+internal struct MockService : ServiceType {
+
+  internal let serverConfig: ServerConfigType
+  internal let oauthToken: OauthTokenAuthType?
+  internal let language: String
+
+  internal init(serverConfig: ServerConfigType = ServerConfig.production, oauthToken: OauthTokenAuthType? = nil, language: String = "en") {
+    self.serverConfig = serverConfig
+    self.oauthToken = oauthToken
+    self.language = language
+  }
+
+  internal func fetchActivities() -> SignalProducer<ActivityEnvelope, ErrorEnvelope> {
+    return SignalProducer(value:
+      ActivityEnvelope(
+        activities: [
+          ActivityFactory.updateActivity,
+          ActivityFactory.backingActivity,
+          ActivityFactory.successActivity
+        ],
+        urls: ActivityEnvelope.UrlsEnvelope(
+          api: ActivityEnvelope.UrlsEnvelope.ApiEnvelope(
+            moreActivities: ""
+          )
+        )
+      )
+    )
+  }
+
+  internal func fetchDiscovery(params: DiscoveryParams) -> SignalProducer<DiscoveryEnvelope, ErrorEnvelope> {
+
+    let projects = (1...10).map { ProjectFactory.live(id: $0 + params.hashValue) }
+
+    return SignalProducer(value:
+      DiscoveryEnvelope(
+        projects: projects,
+        urls: DiscoveryEnvelope.UrlsEnvelope(
+          api: DiscoveryEnvelope.UrlsEnvelope.ApiEnvelope(
+            more_projects: ""
+          )
+        ),
+        stats: DiscoveryEnvelope.StatsEnvelope(
+          count: 200
+        )
+      )
+    )
+  }
+
+  internal func fetchProjects(params: DiscoveryParams) -> SignalProducer<[Project], ErrorEnvelope> {
+    return fetchDiscovery(params)
+      .map { $0.projects }
+  }
+
+  internal func fetchProject(params: DiscoveryParams) -> SignalProducer<Project, ErrorEnvelope> {
+    return fetchDiscovery(params)
+      .map { $0.projects.first }
+      .ignoreNil()
+  }
+
+  internal func fetchProject(project: Project) -> SignalProducer<Project, ErrorEnvelope> {
+    return SignalProducer(value: project)
+  }
+
+  internal func fetchUserSelf() -> SignalProducer<User, ErrorEnvelope> {
+    return .empty
+  }
+
+  internal func fetchUser(user: User) -> SignalProducer<User, ErrorEnvelope> {
+    return SignalProducer(value: user)
+  }
+
+  internal func fetchCategories() -> SignalProducer<[Models.Category], ErrorEnvelope> {
+
+    return SignalProducer(value: [
+      CategoryFactor.art,
+      CategoryFactor.comics,
+      CategoryFactor.illustration,
+      ]
+    )
+  }
+
+  internal func fetchCategory(category: Models.Category) -> SignalProducer<Models.Category, ErrorEnvelope> {
+    return SignalProducer(value: category)
+  }
+
+  internal func toggleStar(project: Project) -> SignalProducer<Project, ErrorEnvelope> {
+    return .init(value: project)
+  }
+
+  internal func star(project: Project) -> SignalProducer<Project, ErrorEnvelope> {
+    return .init(value: project)
+  }
+
+  internal func login(email email: String, password: String) -> SignalProducer<AccessTokenEnvelope, ErrorEnvelope> {
+
+    return SignalProducer(value:
+      AccessTokenEnvelope(
+        access_token: "deadbeef",
+        user: UserFactory.user
+      )
+    )
+  }
+}
