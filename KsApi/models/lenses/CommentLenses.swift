eef9fdf28c428d999e1d2e09e8e92a3cc0c4c1ac
Christopher Wright <christopherwright@gmail.com>
2016-06-28 18:39:20 -0400

    Add author, body, createdAt comment lenses

--- KsApi/models/lenses/CommentLenses.swift
+++ KsApi/models/lenses/CommentLenses.swift
@@ -3,6 +3,21 @@ import Prelude
 
 extension Comment {
   public enum lens {
+    public static let author = Lens<Comment, User>(
+      view: { $0.author },
+      set: { Comment(author: $0, body: $1.body, createdAt: $1.createdAt, deletedAt: $1.deletedAt, id: $1.id) }
+    )
+
+    public static let body = Lens<Comment, String>(
+      view: { $0.body },
+      set: { Comment(author: $1.author, body: $0, createdAt: $1.createdAt, deletedAt: $1.deletedAt, id: $1.id) }
+    )
+
+    public static let createdAt = Lens<Comment, NSTimeInterval>(
+      view: { $0.createdAt },
+      set: { Comment(author: $1.author, body: $1.body, createdAt: $0, deletedAt: $1.deletedAt, id: $1.id) }
+    )
+
     public static let deletedAt = Lens<Comment, NSTimeInterval?>(
       view: { $0.deletedAt },
       set: { Comment(author: $1.author, body: $1.body, createdAt: $1.createdAt, deletedAt: $0, id: $1.id) }
