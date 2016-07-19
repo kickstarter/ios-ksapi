65eed1e8ac5c97bd2eb5b97b035ab27df9e5d217
Stephen Celis <stephen.celis@gmail.com>
2016-07-19 08:58:05 -0400

    Work around a release build Swift compiler bug (#78)
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- KsApi/models/templates/ProjectStatsEnvelopeTemplates.swift
+++ KsApi/models/templates/ProjectStatsEnvelopeTemplates.swift
@@ -1,6 +1,13 @@
 extension ProjectStatsEnvelope {
   internal static let template = ProjectStatsEnvelope(
-    cumulative: .template,
+    // using `.template` causes a segfault in release builds
+    cumulative: ProjectStatsEnvelope.Cumulative(
+      averagePledge: 0,
+      backersCount: 0,
+      goal: 0,
+      percentRaised: 0,
+      pledged: 0
+    ),
     fundingDistribution: [],
     referrerStats: [],
     rewardStats: [.template],
