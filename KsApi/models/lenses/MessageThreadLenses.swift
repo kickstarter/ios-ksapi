d5b25437bf6ab3a434eca52528df2c36dce7fef6
Brandon Williams <mbw234@gmail.com>
2016-12-06 18:33:23 -0500

    Added a few lenses. (#134)

--- KsApi/models/lenses/MessageThreadLenses.swift
+++ KsApi/models/lenses/MessageThreadLenses.swift
@@ -8,5 +8,17 @@ extension MessageThread {
       set: { MessageThread(backing: $1.backing, closed: $1.closed, id: 0, lastMessage: $1.lastMessage,
         participant: $1.participant, project: $1.project, unreadMessagesCount: $1.unreadMessagesCount) }
     )
+
+    public static let participant = Lens<MessageThread, User>(
+      view: { $0.participant },
+      set: { MessageThread(backing: $1.backing, closed: $1.closed, id: $0.id, lastMessage: $1.lastMessage,
+        participant: $0, project: $1.project, unreadMessagesCount: $1.unreadMessagesCount) }
+    )
+
+    public static let project = Lens<MessageThread, Project>(
+      view: { $0.project },
+      set: { MessageThread(backing: $1.backing, closed: $1.closed, id: $0.id, lastMessage: $1.lastMessage,
+        participant: $1.participant, project: $0, unreadMessagesCount: $1.unreadMessagesCount) }
+    )
   }
 }
