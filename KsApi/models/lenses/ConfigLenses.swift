d87f19a748335b1bf70264fdd1666058296e3e57
Brandon Williams <mbw234@gmail.com>
2017-01-14 00:13:32 +0200

    Added feature lens

--- KsApi/models/lenses/ConfigLenses.swift
+++ KsApi/models/lenses/ConfigLenses.swift
@@ -14,9 +14,17 @@ extension Config {
     public static let countryCode = Lens<Config, String>(
       view: { $0.countryCode },
       set: { Config(abExperiments: $1.abExperiments, appId: $1.appId, applePayCountries: $1.applePayCountries,
-        countryCode: $0, features: $1.features, iTunesLink: $1.iTunesLink,
-        launchedCountries: $1.launchedCountries, locale: $1.locale,
-        stripePublishableKey: $1.stripePublishableKey) }
+                    countryCode: $0, features: $1.features, iTunesLink: $1.iTunesLink,
+                    launchedCountries: $1.launchedCountries, locale: $1.locale,
+                    stripePublishableKey: $1.stripePublishableKey) }
+    )
+
+    public static let features = Lens<Config, [String:Bool]>(
+      view: { $0.features },
+      set: { Config(abExperiments: $1.abExperiments, appId: $1.appId, applePayCountries: $1.applePayCountries,
+                    countryCode: $1.countryCode, features: $0, iTunesLink: $1.iTunesLink,
+                    launchedCountries: $1.launchedCountries, locale: $1.locale,
+                    stripePublishableKey: $1.stripePublishableKey) }
     )
 
     public static let launchedCountries = Lens<Config, [Project.Country]>(
