4ea66535f4f62bd131347843b5f93de73576b2de
Justin Swart <jwswart@gmail.com>
2017-01-31 10:27:18 +0200

    Initialise template dates with constant timestamp

--- KsApi/models/templates/CommentTemplates.swift
+++ KsApi/models/templates/CommentTemplates.swift
@@ -2,7 +2,7 @@ extension Comment {
   internal static let template = Comment(
     author: .template,
     body: "Exciting!",
-    createdAt: Date().timeIntervalSince1970,
+    createdAt: Date(timeIntervalSince1970: 1475361315).timeIntervalSince1970,
     deletedAt: nil,
     id: 1
   )
